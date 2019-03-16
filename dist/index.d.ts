e554df6ca535ea221bf71d45ef1bbf07b658293c
daishi <daishi@axlight.com>
2019-03-17 01:28:00 +0900

    possible fix for #8

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -14,6 +14,7 @@ export type SetGlobalState<S> = <N extends keyof S>(
 export type HookResult<T> = [T, (u: Update<T>) => void];
 
 export type Reducer<S, A> = (state: S, action: A) => S;
+export type ReduxLikeReducer<S, A> = (state: S | undefined, action: A) => S;
 
 export type Dispatch<A> = (action: A) => A;
 
@@ -43,6 +44,11 @@ export type CreateStore = <S, A>(
   initialState: S,
   enhancer?: Enhancer<S, A> | AnyEnhancer,
 ) => Store<S, A>;
+export type CreateReduxLikeStore = <S, A>(
+  reducer: ReduxLikeReducer<S, A>,
+  initialState: S | undefined,
+  enhancer?: Enhancer<S, A> | AnyEnhancer,
+) => Store<S, A>;
 
 export const createGlobalState: CreateGlobalState;
-export const createStore: CreateStore;
+export const createStore: CreateStore | CreateReduxLikeStore;
