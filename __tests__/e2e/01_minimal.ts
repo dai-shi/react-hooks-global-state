ef35603022e89871599dcbb351e6ba858ca55108
daishi <daishi@axlight.com>
2020-02-15 16:06:33 +0900

    e2e/02_typescript

--- __tests__/e2e/01_minimal.ts
+++ __tests__/e2e/01_minimal.ts
@@ -1,22 +1,21 @@
 /* global page */
 
-const port = process.env.PORT || '8080';
 jest.setTimeout(15 * 1000);
 
 describe('01_minimal', () => {
   it('should work with events', async () => {
-    await page.goto(`http://localhost:${port}/`);
+    await page.goto(`http://localhost:${process.env.PORT || '8080'}/`);
 
-    await page.click('div:nth-of-type(1) button:nth-of-type(1)');
+    await page.click('div:nth-of-type(1) > button:nth-of-type(1)');
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
-    await page.click('div:nth-of-type(1) button:nth-of-type(2)');
+    await page.click('div:nth-of-type(1) > button:nth-of-type(2)');
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
-    await page.click('div:nth-of-type(2) button:nth-of-type(1)');
+    await page.click('div:nth-of-type(2) > button:nth-of-type(1)');
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
-    await page.click('div:nth-of-type(2) button:nth-of-type(2)');
+    await page.click('div:nth-of-type(2) > button:nth-of-type(2)');
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchSnapshot();
 
     await page.type('div:nth-of-type(3) > input', '1');
